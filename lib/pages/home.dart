53bf3a5ca6e3a4fd66949dc7c075051a0983edba
harrisking <llc13588440410@outlook.com>
2026-02-10 11:16:59 +0800

    feat(home): 添加计数器功能并重构为有状态组件
    将首页从无状态组件重构为有状态组件，并添加计数器功能

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _count = 0;
+
+  void _increateCount() {
+    setState(() => _count++);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('首页内容'));
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '当前计数: $_count',
+              style: TextStyle(fontSize: 24, color: Colors.pink),
+            ),
+            SizedBox(height: 20), // 用于在文本和按钮之间留出 20 像素的垂直间距
+            ElevatedButton(
+              onPressed: _increateCount,
+              child: Text(
+                '增加',
+                style: TextStyle(fontSize: 16, color: Colors.pink),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
