23fdfef41304f8278a850bd8920e151b86e8691a
harrisking <llc13588440410@outlook.com>
2026-02-10 14:12:00 +0800

    feat(组件): 添加TodoItem组件并集成到个人中心页面
    实现一个可勾选的待办事项组件，并在个人中心页面中使用ListView展示

---
+++ lib/pages/components/todo_item.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class TodoItem extends StatelessWidget {
+  final String content;
+  final bool status;
+  Function(bool?)? onChanged;
+
+  TodoItem({
+    super.key,
+    required this.content,
+    required this.status,
+    required this.onChanged,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 0, right: 0, bottom: 14),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Row(
+          children: [
+            Checkbox(value: status, onChanged: (value) => onChanged!(value)),
+            Text(
+              content,
+              style: TextStyle(
+                decoration: status ? TextDecoration.lineThrough : null,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/my.dart
+++ lib/pages/my.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo_1/pages/components/todo_item.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List _todoItems = [
+    ['今日任务', false],
+  ];
+
+  void changeStatus(int index) {
+    setState(() {
+      _todoItems[index][1] = !_todoItems[index][1];
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('个人中心内容')));
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(14),
+        child: ListView.builder(
+          itemCount: _todoItems.length,
+          itemBuilder: (context, index) => TodoItem(
+            content: _todoItems[index][0],
+            status: _todoItems[index][1],
+            onChanged: (value) => changeStatus(index),
+          ),
+        ),
+      ),
+    );
   }
 }
