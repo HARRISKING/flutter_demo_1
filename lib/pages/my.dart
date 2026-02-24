c6e39b639d1a733b50bc421fad4cdf3072afa494
harrisking <llc13588440410@outlook.com>
2026-02-24 09:30:08 +0800

    feat(悬浮按钮): 添加可拖拽的悬浮按钮功能
    实现悬浮按钮的拖拽功能，使其可以自由移动并自动吸附到屏幕两侧

--- lib/pages/my.dart
+++ lib/pages/my.dart
@@ -17,6 +17,9 @@ class MyApp extends StatefulWidget {
 
 // MyApp的状态管理类，继承自State
 class _MyAppState extends State<MyApp> {
+  late Offset _buttonPosition;
+  late double _screenWidth;
+
   // TODO任务列表，初始包含一个任务
   List _todoItems = [
     ['今日任务', false], // [任务内容, 完成状态]
@@ -40,9 +43,41 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+  void initState() {
+    super.initState();
+    _buttonPosition = Offset(0, 0);
+  }
+
+  void _updatePosition(DraggableDetails details) {
+    // 获取拖拽结束的 x 坐标
+    double newX = details.offset.dx;
+    // 获取拖拽结束的 y 坐标
+    double newY = details.offset.dy;
+    newX = newX.clamp(0, _screenWidth + 56);
+    newY = newY.clamp(168, MediaQuery.of(context).size.height - 300);
+
+    setState(() {
+      _buttonPosition = Offset(newX, newY);
+    });
+    if (newX < _screenWidth / 2) {
+      _buttonPosition = Offset(28, newY);
+    } else {
+      _buttonPosition = Offset(_screenWidth - 56, newY);
+    }
+  }
+
   // 重写build方法，构建UI界面
   @override
   Widget build(BuildContext context) {
+    // 获取屏幕高度
+    double _screenHeight = MediaQuery.of(context).size.height;
+    _screenWidth = MediaQuery.of(context).size.width;
+
+    // 首次构建时设置默认位置为右下角
+    if (_buttonPosition == Offset(0, 0)) {
+      // 计算右下角位置，留适当边距
+      _buttonPosition = Offset(_screenWidth - 56, _screenHeight - 500);
+    }
     // 返回Scaffold组件，提供应用程序的基本结构
     return Scaffold(
       // 页面主体内容
@@ -64,17 +99,34 @@ class _MyAppState extends State<MyApp> {
           ),
         ),
       ),
+
       // 悬浮操作按钮
-      floatingActionButton: FloatingActionButton(
-        // 点击事件回调，显示添加任务对话框
-        onPressed: () => showDialog(
-          // 上下文
-          context: context,
-          // 对话框构建器，返回AddDialog组件
-          builder: (context) => AddDialog(onAddTask: addTask),
-        ),
-        // 按钮图标
-        child: Icon(Icons.add),
+      floatingActionButton: Stack(
+        children: [
+          Positioned(
+            left: _buttonPosition.dx,
+            top: _buttonPosition.dy,
+            child: Draggable(
+              feedback: FloatingActionButton(
+                // 反馈组件不可点击
+                onPressed: null,
+                // 提示文本
+                tooltip: 'Increment',
+                // 显示加号图标
+                child: const Icon(Icons.add),
+              ),
+              onDragEnd: _updatePosition,
+              childWhenDragging: Container(),
+              child: FloatingActionButton(
+                onPressed: () => showDialog(
+                  context: context,
+                  builder: (context) => AddDialog(onAddTask: addTask),
+                ),
+                child: Icon(Icons.add),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
